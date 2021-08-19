a3d520d7a6f08cad8d50c7325949b467db2dcba4
YBolshakova <y.shemanska@gmail.com>
2021-08-19 14:37:54 +0300

    no message

--- home-task-1/task3.js
+++ home-task-1/task3.js
@@ -1,4 +1,9 @@
 function solveQuadraticEquation(a,b,c){
-    // code here
+    let x1, x2;
+    let discriminant = b * b - 4 * a * c;   
+        x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        x2 = (-b - Math.sqrt(discriminant)) / (2 * a);  
+        return x1 + ',' + x2;         
     }
+    console.log(solveQuadraticEquation(1,-1,-2));
     
